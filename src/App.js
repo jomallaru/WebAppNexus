97c3dabaf4980f4a825571d836dde46801ac0cbe
jomallaru <jomallaru@gmail.com>
2023-11-29 17:43:47 -0500

    Segundo Update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import Home from './Screens/Home';
-import Services from './Screens/Services';
-import HowWeWork from './Screens/HowWeWork';
-import Benefits from './Screens/Benefits';
-import Contact from './Screens/Contact';
-import NavBar from './Components/NavBar';
+import Home from "./Screens/Home";
+import Services from "./Screens/Services";
+import HowWeWork from "./Screens/HowWeWork";
+import Benefits from "./Screens/Benefits";
+import Contact from "./Screens/Contact";
+import NavBar from "./Components/NavBar";
+
 
 function App() {
   return ( <div className="App">
--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -3,48 +3,63 @@ import { Link } from 'react-scroll';
 import styles from "./NavBar.module.css";
 import { CgMenuGridR } from "react-icons/cg";
 import { AiTwotoneCloseSquare } from "react-icons/ai";
+import { useScrollPosition } from "../Hooks/scrollPosition";
 
 const NavBar = () => {
-    const [navBarOpen, setNavBarOpen] = useState(false);
-    const [windowDimension, setWindowDimension] = useState({
-        width: window.innerWidth,
-        height: window.innerHeight,
+  const [navBarOpen, setNavBarOpen] = useState(false);
+  const [windowDimension, setWindowDimension] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
+
+  const detectDimension = () => {
+    setWindowDimension({
+      width: window.innerWidth,
+      height: window.innerHeight,
     });
+  };
 
-    const detectDimension = () => {
-        setWindowDimension({
-            width: window.innerWidth,
-            height: window.innerHeight
-        })
-    }
-    useEffect(() => {
-        window.addEventListener('resize', detectDimension)
-        windowDimension.width > 800 && setNavBarOpen(false)
-        return () => {
-            removeEventListener('resize', detectDimension)
-        }
-    }, [windowDimension])
+   useEffect(() => {
+    window.addEventListener("resize", detectDimension);
+    windowDimension.width > 800 && setNavBarOpen(false);
+    return () => {
+      window.removeEventListener("resize", detectDimension);
+    };
+  }, [windowDimension]);
 
-    const links = [
-        {
-            id: 1,
-            link: "Home",
-        },
-        {
-            id: 2,
-            link: "Services",
-        },
-        {
-            id: 3,
-            link: "HowWeWork",
-        },
-        {
-            id: 4,
-            link: "Benefits",
-        },
-    ];
+
+     const links = [
+    {
+      id: 1,
+      link: "Home",
+    },
+    {
+      id: 2,
+      link: "Services",
+    },
+    {
+      id: 3,
+      link: "HowWeWork",
+    },
+    {
+      id: 4,
+      link: "Benefits",
+    },
+  ];
+
+  const scrollPosition = useScrollPosition();
+
     return (
-        <div className={!navBarOpen ? styles.navBar : styles.navOpen}>
+        
+    <div
+      className={
+        navBarOpen
+          ? styles.navOpen
+          : scrollPosition > 0
+          ? styles.navOnScroll
+          : styles.navBar
+      }
+    >
             {!navBarOpen && <p className={styles.logo}>WebApp nexus| Desarrollo de Aplicaciones Web</p>}
             {!navBarOpen && windowDimension.width < 800 ? (
                 <CgMenuGridR onClick={() => setNavBarOpen(!navBarOpen)} size={30} />
@@ -58,26 +73,53 @@ const NavBar = () => {
                     />
                 )
             )}
-            {navBarOpen ||
-                (windowDimension.width < 800 && (
-                    <ul className={styles.linksContainer}>
-                        {links.map((x) => (
-                            <div>
-                                <Link
-                                    onClick={() => setNavBarOpen(false)}
-                                    to={x.link}
-                                    smooth
-                                    duration={500}
-                                    className={styles.navLink}
-                                >{x.link === "HowWeWork" ? "How We Work" : x.link}
-                                </Link>
-                                <div className={styles.border}></div>
-                            </div>
-                        ))}
-                    </ul>
-                ))}
-        </div>
-    );
+            {navBarOpen && (
+        <ul className={styles.linksContainer}>
+          {links.map(({ id, link }) => (
+            <div>
+              <Link
+                key={id}
+                onClick={() => setNavBarOpen(false)}
+                to={link}
+                smooth
+                duration={500}
+                className={styles.navLink}
+              >
+                {link === "HowWeWork" ? "How we work" : link}
+              </Link>
+              <div className={styles.border}></div>
+            </div>
+          ))}
+        </ul>
+      )}
+      {windowDimension.width > 800 && (
+        <ul className={styles.linksContainer}>
+          {links.map(({ link, id }) => (
+            <div>
+              <Link
+                onClick={() => setNavBarOpen(false)}
+                to={link}
+                smooth
+                duration={500}
+                className={styles.navLink}
+              >
+                {link === "HowWeWork" ? "How we work" : link}
+              </Link>
+              <div className={styles.border}></div>
+            </div>
+          ))}
+          <Link
+            onClick={() => setNavBarOpen(false)}
+            to="Contact"
+            smooth
+            duration={500}
+            className={styles.contactLink}
+          >
+            Contact
+          </Link>
+        </ul>
+      )}
+    </div>
+  );
 };
-
 export default NavBar;
