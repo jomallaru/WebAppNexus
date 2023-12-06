d758d5274a4d2d477f74eda23c31f80f1b800925
jomallaru <jomallaru@gmail.com>
2023-12-06 13:32:19 -0500

    Update 6/12/2023

--- src/Screens/Services.js
+++ src/Screens/Services.js
@@ -1,27 +1,62 @@
-import React from "react";
-import styles from "./Services.module.css";
+// Services.js
+import React, { useState } from 'react';
+import styles from './Services.module.css';
+
+// Importa las imágenes desde la carpeta assets
+import image1 from '../assets/app.jpg';
+import image2 from '../assets/web.jpg';
+import image3 from '../assets/Soporte.jpg';
 
 const Services = () => {
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const totalSlides = 3;
+
+  const nextSlide = () => {
+    setCurrentSlide((prevSlide) => (prevSlide === totalSlides - 1 ? 0 : prevSlide + 1));
+  };
+
+  const prevSlide = () => {
+    setCurrentSlide((prevSlide) => (prevSlide === 0 ? totalSlides - 1 : prevSlide - 1));
+  };
+
+  const slideData = [
+    {
+      image: image1,
+      title: 'Aplicación',
+      description: 'Desarrollamos aplicaciones personalizadas para tus necesidades.',
+    },
+    {
+      image: image2,
+      title: 'Sitio web',
+      description: 'Creamos sitios web atractivos y funcionales para tu negocio.',
+    },
+    {
+      image: image3,
+      title: 'Soporte',
+      description: 'Brindamos soporte técnico para garantizar el funcionamiento de tus sistemas.',
+    },
+    // Agrega más datos para más slides según sea necesario
+  ];
+
   return (
-    <div name="Services" className={styles.services}>      
+    <div name="Services" className={styles.services}>
       <p className={styles.textTitle}>Nuestros Productos y Servicios</p>
-      <div className={styles.cardContainer}>
-        <div className={styles.card}>
-          <h2>Apliaciones Web</h2>
-          <p>Creamos aplicaciones web personalizadas que satisfacen las necesidades específicas de nuestros clientes, ofreciendo funcionalidad y experiencia de usuario excepcionales, desde plataformas de comercio electrónico hasta sistemas de gestión empresarial.</p>
+      <div className={styles.slider}>
+        <div className={styles['slide-container']} style={{ transform: `translateX(-${currentSlide * 100}%)` }}>
+          {slideData.map((slide, index) => (
+            <div className={styles.slide} key={index}>
+              <img src={slide.image} alt={`Slide ${index + 1}`} />
+              <div className={styles['slide-content']}>
+                <h2>{slide.title}</h2>
+                <p>{slide.description}</p>
+              </div>
+            </div>
+          ))}
         </div>
-
-        <div className={styles.card}>
-          <h2>Paginas Web</h2>
-          <p>Crearemos sitios web visualmente atractivos y funcionales que representan la identidad única de cada cliente. Nuestro enfoque se centra en la usabilidad, el diseño receptivo y la optimización para motores de búsqueda, garantizando una presencia en línea efectiva.</p>
-        </div>
-
-        <div className={styles.card}>
-          <h2>Soporte y Mantenimiento</h2>
-          <p>Ofrecemos soporte técnico y mantenimiento integral para garantizar el rendimiento y seguridad de aplicaciones y sitios web.</p>
-        </div>
+        <button onClick={prevSlide}>Previous</button>
+        <button onClick={nextSlide}>Next</button>
+        <p>Solucionamos los problemas de tu empresa creando increíbles Apliaciones y páginas web</p>
       </div>
-      <p>Solucionamos los problemas de tu empresa creando increíbles Apliaciones y páginas web</p>
     </div>
   );
 };
---
+++ src/assets/data.js
@@ -0,0 +1,12 @@
+export const data = [
+    {
+      id: 1, imgUrl: "src/assets/web.jpg"
+    },
+    {
+      id: 2, imgUrl: "./web.jpg"
+    },
+    {
+      id: 3, imgUrl: "./Soporte.jpg"
+    }
+  ];
+
